5b80bca9cb8ef102863c244a11c703a7d21dfc8e
Jaymi Yam <jaymiyam@gmail.com>
2025-11-02 13:02:40 +0800

    features grid fade in animation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import * as THREE from 'three';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { createHeroScene } from './heroScene';
 import { createColorConfigScene } from './colorConfigScene';
 
+gsap.registerPlugin(ScrollTrigger);
+
+// Three.js master loading manager
 const loadingManager = new THREE.LoadingManager(() => {
   const preloader = document.querySelector('.preloader');
   preloader.classList.add('hide');
@@ -10,5 +15,21 @@ const loadingManager = new THREE.LoadingManager(() => {
   }, 600);
 });
 
+// Initiate Three.js scenes
 createHeroScene(loadingManager);
 createColorConfigScene(loadingManager);
+
+// Features section animations
+document.querySelectorAll('.bento-cell').forEach((cell) => {
+  const tween = gsap.from(cell, {
+    opacity: 0,
+    y: 50,
+  });
+
+  ScrollTrigger.create({
+    animation: tween,
+    trigger: cell,
+    start: 'top 75%',
+    toggleActions: 'play none none reverse',
+  });
+});
